c5366b2530b2e51fc01f86fcc7dc11e6e4509f3d
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-15 23:14:08 +0530

    fix: login.js and logout.js

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -1,7 +1,6 @@
-import express from  'express';
-import bcrypt from 'bcrypt';
+import express from "express";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
 
 const router = express.Router();
 
--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -1,12 +1,11 @@
-import express from 'express';
-
+import express from "express";
 
 const router = express.Router();
 
 router.post("/logout", (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: false // set true if you're using HTTPS
+    secure: false, // set true if you're using HTTPS
   });
   res.send("Logged out successfully!");
 });
