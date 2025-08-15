cdf6e4e4974cf433434e40b0519c5e22ecd15a71
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-08-15 22:37:38 +0530

    feat: implement server setup with user request handling and database connection

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import connectDB from "./config/database.js";
+import User from "./model/User.js";
+import cookieParser from "cookie-parser";
+import UserAuth from "./middeleware/UserAuth.js";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.post("/sendRequest", UserAuth, async (req, res) => {
+  const user = req.user;
+  console.log("Sending Connection Request");
+  res.send("Connection sent :" + user.name);
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Error fetching feed: " + err.message);
+  }
+});
+
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+
+    app.listen(7000, () => {
+      console.log("Server is successfully listening on port 7000...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!", err);
+  });
